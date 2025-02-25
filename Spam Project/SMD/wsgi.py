3786b3ceb6ca6781aadfe8d60145e51dcc6a8763
UnDeFined02 <marufmujawar11@gmail.com>
2025-02-25 12:23:21 +0530

    Update wsgi.py

--- Spam Project/SMD/wsgi.py
+++ Spam Project/SMD/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for SMD project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+# Load environment variables from .env file
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SMD.settings')
 
 application = get_wsgi_application()
+
